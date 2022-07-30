0cf25babde2c7519eaa95238b59df3b4223233cd
anglal <lalshwarlimbu@gmail.com>
2022-07-29 22:24:07 -0700

    adding some common problems

--- src/problems1/Pattern.java
+++ src/problems1/Pattern.java
@@ -1,5 +1,7 @@
 package problems1;
 
+import java.util.Scanner;
+
 class Patterns {
 	// 1.
 	public static void pattern1(int n, int m) {
@@ -127,43 +129,93 @@ class Patterns {
 			counter++;
 		}
 	}
-
+	
+	// 11)	Write a program to check if a given number is Positive, Negative, or Zero.
+	public static void findPositive(int x) {
+		if(x > 0) {
+			System.out.println("Then number is positive");
+		}else if(x == 0) {
+			System.out.println("The number is zero");
+		}else {
+			System.out.println("The number is negative");
+		}
+	}
+	
+	// 12) Write a program to check if a given number is odd or even.
+	public static void findOdd(int data) {
+		if(data % 2 == 0) {
+			System.out.println("The number is even.");
+		}else {
+			System.out.println("The number is odd.");
+		}
+	}
+	// 13) Write a program to check if the program has received command line arguments or not. If the program has not received 
+	//	the values then print "No Values", else print all the values in a single line separated by ,(comma). Eg1) 
+	//	java Example O/P: No values Eg2) java Example Mumbai Bangalore O/P: Mumbai,Bangalore [Note: You can use length property 
+	//	of an array to check its length
+	public static void checkCommandLine() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter words");
+		String input = scanner.nextLine();
+		if(input.isBlank()) {
+			System.out.println("No inputs received");
+		}else {
+			String[] inputs = input.split("\\s+");
+			for(int i = 0; i< inputs.length; i++) {
+				System.out.print(inputs[i]);
+				if(i < inputs.length -1) {
+					System.out.print(", ");
+				}
+			}
+		}
+		
+		scanner.close();
+	}
 }
 
 public class Pattern {
 
 	public static void main(String[] args) {
-		System.out.println("\n-----------------------------------------------");
-//		1. 
-		Patterns.pattern1(3,5);
-		System.out.println("\n-----------------------------------------------");
-//		2.
-		Patterns.pattern2(4);
-		System.out.println("\n-----------------------------------------------");
-//		3.
-		Patterns.pattern3(5);
-		System.out.println("\n-----------------------------------------------");
-//		4
-		Patterns.pattern4(7);
-		System.out.println("\n-----------------------------------------------");
-//		5
-		Patterns.pattern5(5);
-		System.out.println("\n-----------------------------------------------");
-//		6
-		Patterns.pattern6(5);
-		System.out.println("\n-----------------------------------------------");
-//		7
-		Patterns.pattern7(50, 5);
-		System.out.println("\n-----------------------------------------------");
-//		8 Odd numbers
-		Patterns.pattern8(50, 5);
-		System.out.println("\n-----------------------------------------------");
-//		9 Even numbers
-		Patterns.pattern9(50, 5);
-		System.out.println("\n-----------------------------------------------");
-//		10
-		Patterns.pattern10('z');
-		System.out.println("\n-----------------------------------------------");
+//		System.out.println("\n-----------------------------------------------");
+////		1. 
+//		Patterns.pattern1(3,5);
+//		System.out.println("\n-----------------------------------------------");
+////		2.
+//		Patterns.pattern2(4);
+//		System.out.println("\n-----------------------------------------------");
+////		3.
+//		Patterns.pattern3(5);
+//		System.out.println("\n-----------------------------------------------");
+////		4
+//		Patterns.pattern4(7);
+//		System.out.println("\n-----------------------------------------------");
+////		5
+//		Patterns.pattern5(5);
+//		System.out.println("\n-----------------------------------------------");
+////		6
+//		Patterns.pattern6(5);
+//		System.out.println("\n-----------------------------------------------");
+////		7
+//		Patterns.pattern7(50, 5);
+//		System.out.println("\n-----------------------------------------------");
+////		8 Odd numbers
+//		Patterns.pattern8(50, 5);
+//		System.out.println("\n-----------------------------------------------");
+////		9 Even numbers
+//		Patterns.pattern9(50, 5);
+//		System.out.println("\n-----------------------------------------------");
+////		10
+//		Patterns.pattern10('z');
+//		System.out.println("\n-----------------------------------------------");
+////		11
+//		Patterns.findPositive(1222);
+//		System.out.println("\n-----------------------------------------------");
+////		12
+//		Patterns.findOdd(9);
+//		System.out.println("\n-----------------------------------------------");
+////		13
+//		Patterns.checkCommandLine();
+//		System.out.println("\n-----------------------------------------------");
 
 	}
 
